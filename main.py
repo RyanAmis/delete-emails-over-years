29f384e2c0e4c3d21b7844f06e41f4bbbbd57839
RyanAmis <118458603+RyanAmis@users.noreply.github.com>
2023-01-31 15:21:51 +0000

    update

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import os
 import win32com.client
 
+# Connecting to outlook
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6)
+root_folder = inbox.Folders(6)
+messages = root_folder.Items
+
+
+
